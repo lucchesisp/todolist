66ee9947759363d14d20b3dad43f68b55dd8b36d
Gabriel Lucchesi <lucchesisp@gmail.com>
2022-03-20 02:14:27 -0300

    Mudamos o formato de adicionar tarefas, para que fique persistente, e adicionamos uma funcionalidade de remover

--- funcionalidades.js
+++ funcionalidades.js
@@ -1,4 +1,36 @@
-document.querySelector('#add').onclick = function() {
+var tarefas = [];
+
+function removerTarefa(posicao) {
+    tarefas.splice(posicao, 1);
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+
+    document.querySelector('#tarefas').innerHTML = '';
+    tarefas.forEach((tarefa, posicao) => {
+        document.querySelector('#tarefas').innerHTML += `
+            <div class="tarefa" onClick="removerTarefa(${posicao})">
+                <span>${tarefa}</span>
+            </div>
+        `;
+    });
+}
+
+function recuperaTarefas() {
+
+    var tarefasSalvas = JSON.parse(localStorage.getItem('tarefas'))
+
+    if(tarefasSalvas !== null) {
+        tarefas = tarefasSalvas
+        tarefas.forEach((tarefa, posicao) => {
+            document.querySelector('#tarefas').innerHTML += `
+                <div class="tarefa" onClick="removerTarefa(${posicao})">
+                    <span>${tarefa}</span>
+                </div>
+            `;
+        });
+    }
+}
+
+function salvaTarefas() {
     var minhaTarefa = document.querySelector('#tarefa').value
 
     if(minhaTarefa.length == 0) {
@@ -6,11 +38,27 @@ document.querySelector('#add').onclick = function() {
         return
     }
 
+    if(tarefas != null) {
+        tarefasTamanho = tarefas.length;
+    } else {
+        tarefasTamanho = 0
+    }
+
+    tarefas.push(minhaTarefa)
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+
     document.querySelector('#tarefas').innerHTML += `
-        <div class="tarefa">
+        <div class="tarefa" onClick="removerTarefa(${tarefasTamanho})">
             <span>${minhaTarefa}</span>
         </div>
     `;
 
     document.querySelector('#tarefa').value = ''
+}
+
+recuperaTarefas()
+
+document.querySelector('#add').onclick = function() {
+    salvaTarefas();
 }
